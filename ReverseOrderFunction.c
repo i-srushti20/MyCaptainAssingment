953a9b3d1df307def37cf82e7dab301ea1fdc9bf
i-srushti20 <90465814+i-srushti20@users.noreply.github.com>
2021-09-24 21:56:18 +0530

    Add files via upload

---
+++ ReverseOrderFunction.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+ {
+	
+    char str[50] = "My_Captain_C_Programming_!!";
+
+    printf("The given string is = %s\n",str);
+    
+    printf("After reversing string is = %s",strrev(str)); //strrev() function
+
+    return 0;
+
+ }
+
